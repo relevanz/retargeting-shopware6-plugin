7851204a5a07a03e42624040ffb0eea923c75863
mba <mba@mba-Aspire-E5-575>
2020-03-30 11:18:23 +0200

    improment for product-export

--- src/Controller/StorefrontController.php
+++ src/Controller/StorefrontController.php
@@ -3,17 +3,9 @@
 namespace Releva\Retargeting\Shopware\Controller;
 
 use Releva\Retargeting\Base\Credentials;
-use Releva\Retargeting\Base\Export\Item\ProductExportItem;
-use Releva\Retargeting\Base\Export\ProductCsvExporter;
-use Releva\Retargeting\Base\Export\ProductJsonExporter;
-use Releva\Retargeting\Shopware\Internal\RepositoryHelper;
-use Releva\Retargeting\Shopware\Internal\ShopInfo;
+use Releva\Retargeting\Base\Exception\RelevanzException;
+use Releva\Retargeting\Shopware\Internal\ProductExporter;
 
-use Shopware\Core\Checkout\Cart\LineItem\LineItem;
-use Shopware\Core\Checkout\Cart\SalesChannel\CartService;
-use Shopware\Core\Content\Product\Cart\ProductLineItemFactory;
-use Shopware\Core\Content\ProductEntity;
-use Shopware\Core\Defaults;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope; // need for anotations
 use Shopware\Storefront\Controller\StorefrontController as ShopwareStorefrontController;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
@@ -30,11 +22,6 @@ class StorefrontController extends ShopwareStorefrontController
 {
     
     private const ITEMS_PER_PAGE = 50;
-    
-    /**
-     * @var [(string) sales-channel-id[(string) category-id, ], ]
-     */
-    private $salesChannelCategories = [];
     
     /**
      * @Route("/releva/retargeting/callback", name="frontend.releva.retargeting.callback", options={"seo"="false"}, methods={"GET"})
@@ -67,60 +54,22 @@ class StorefrontController extends ShopwareStorefrontController
     public function productsAction(Request $request, SalesChannelContext $salesChannelContext) : void
     {
         $this->checkCredentials($request, $salesChannelContext);
-        $page = (int) $request->get('page') < 1 ? null : (int) $request->get('page') - 1;
-        $productsSearchResult = $this->get(RepositoryHelper::class)->getProducts(
-            $salesChannelContext->getContext(),
-            ['categories', 'translations', ],
-            [RepositoryHelper::FILTER_PRODUCT_AVAILABLE, ],
-            $page === null ? null : self::ITEMS_PER_PAGE,
-            $page === null ? null : $page * self::ITEMS_PER_PAGE
-        );
-        if ($productsSearchResult->getTotal() === 0) {
-            http_response_code(404);
-            die;
-        }
-        $exporter = $request->get('format') === 'json' ? new ProductJsonExporter() : new ProductCsvExporter();
-        /* @var $cartService CartService */
-        $cartService = $this->get(CartService::class);
-        $detailUrl = $request->getSchemeAndHttpHost().'/detail/%s';//default routing from symfony to product-detail-page
-        $salesChannelCategoryIds = $this->getSalesChannelCategoryIds($salesChannelContext);
-        foreach ($productsSearchResult as $product) {
-            /* @var $product ProductEntity */
-            $productCategoryIds = [];
-            foreach ($product->getCategories() as $productCategory) {
-                $categoryIds = array_filter(explode('|', $productCategory->getPath().'|'.$productCategory->getId()));
-                if (array_intersect($categoryIds, $salesChannelCategoryIds)){
-                    $productCategoryIds[] = $productCategory->getId();
-                }
-            }
-            // create cart and fill with one product to get calculated price
-            $lineItem = (new ProductLineItemFactory)->create($product->getId());
-            $cartService->add($cartService->createNew('releva'), $lineItem, $salesChannelContext);
-            $price = $priceOffer = $lineItem->getPrice()->getTotalPrice();
-            foreach ($cartService->getCart('releva', $salesChannelContext)->getLineItems()->fmap(function (LineItem $lineItem) {
-                return $lineItem->getType() === 'promotion' ? $lineItem : false;
-            }) as $promotionLineItem) {
-                $priceOffer += $promotionLineItem->getPrice()->getTotalPrice();//promotion has negative price
-            }
-            $defaultTranslation = $product->getTranslations()->get($product->getId().'-'.Defaults::LANGUAGE_SYSTEM);
-            $exporter->addItem(
-                new ProductExportItem(
-                    (string) $product->getId(),
-                    (array) $productCategoryIds,
-                    (string) ($product->getName() ?: $defaultTranslation->getName()),
-                    (string) ($product->getMetaDescription() ?: $defaultTranslation->getMetaDescription()),
-                    (string) ($product->getDescription() ?: $defaultTranslation->getDescription()),
-                    (float) $price,
-                    (float) $priceOffer,
-                    (string) sprintf($detailUrl, $product->getId()),
-                    (string) $lineItem->getCover()->getUrl()
-                )
+        try {
+            $page = (int) $request->get('page') < 1 ? null : (int) $request->get('page') - 1;
+            $productExporter = $this->get(ProductExporter::class);
+            $exporter = $productExporter->export(
+                $salesChannelContext,
+                $request->get('format') === 'json' ? ProductExporter::FORMAT_JSON : ProductExporter::FORMAT_CSV,
+                $page === null ? null : self::ITEMS_PER_PAGE,
+                $page === null ? null : $page * self::ITEMS_PER_PAGE
             );
+            foreach ($exporter->getHttpHeaders() as $headerKey => $headerValue) {
+                header(sprintf('%s:%s', $headerKey, $headerValue));
+            }
+            echo $exporter->getContents();
+        } catch (\Exception $exception) {
+            http_response_code($exception instanceof RelevanzException && $exception->getCode() === 1585554289 ? 400 : 500);
         }
-        foreach ($exporter->getHttpHeaders() as $headerKey => $headerValue) {
-            header(sprintf('%s:%s', $headerKey, $headerValue));
-        }
-        echo $exporter->getContents();
         die;
     }
     
@@ -139,23 +88,6 @@ class StorefrontController extends ShopwareStorefrontController
         }
         http_response_code(401);
         die;
-    }
-    
-    private function getSalesChannelCategoryIds (SalesChannelContext $salesChannelContext): array {
-        $salesChannel = $salesChannelContext->getSalesChannel();
-        if (!array_key_exists($salesChannel->getId(), $this->salesChannelCategories)) {
-            $this->salesChannelCategories[$salesChannel->getId()] = [];
-            foreach ([
-                $salesChannel->getNavigationCategoryId(),
-                $salesChannel->getFooterCategoryId(),
-                $salesChannel->getServiceCategoryId(),
-            ] as $categoryId) {
-                if ($categoryId !== null && !in_array($categoryId, $this->salesChannelCategories[$salesChannel->getId()])) {
-                    $this->salesChannelCategories[$salesChannel->getId()][] = $categoryId;
-                }
-            }
-        }
-        return $this->salesChannelCategories[$salesChannel->getId()];
     }
     
 }
---
+++ src/Internal/ProductExporter.php
@@ -0,0 +1,129 @@
+<?php declare(strict_types=1);
+
+namespace Releva\Retargeting\Shopware\Internal;
+
+use Releva\Retargeting\Base\Export\ProductCsvExporter;
+use Releva\Retargeting\Base\Export\ExporterInterface;
+use Releva\Retargeting\Base\Export\ProductJsonExporter;
+use Releva\Retargeting\Base\Export\Item\ProductExportItem;
+use Releva\Retargeting\Shopware\Internal\RepositoryHelper;
+
+use Psr\Container\ContainerInterface;
+
+use Shopware\Core\Defaults;
+use Shopware\Core\Checkout\Cart\LineItem\LineItem;
+use Shopware\Core\Checkout\Cart\SalesChannel\CartService;
+use Shopware\Core\Content\Product\Cart\ProductLineItemFactory;
+use Shopware\Core\System\SalesChannel\SalesChannelEntity;
+use Shopware\Core\Content\Product\ProductEntity;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+
+class ProductExporter
+{
+    private $container;
+    
+    const FORMAT_CSV = 'csv';
+    
+    const FORMAT_JSON = 'json';
+    
+    private $salesChannelCategories = [];
+    
+    private $productTranslations = [];
+
+    public function __construct(ContainerInterface $container) {
+        $this->container = $container;
+    }
+    
+    private function getProductCategoryIds (ProductEntity $product, SalesChannelEntity $salesChannel):array
+    {
+        if (!array_key_exists($salesChannel->getId(), $this->salesChannelCategories)) {
+            $this->salesChannelCategories[$salesChannel->getId()] = [];
+            foreach ([
+                $salesChannel->getNavigationCategoryId(),
+                $salesChannel->getFooterCategoryId(),
+                $salesChannel->getServiceCategoryId(),
+            ] as $categoryId) {
+                if ($categoryId !== null && !in_array($categoryId, $this->salesChannelCategories[$salesChannel->getId()])) {
+                    $this->salesChannelCategories[$salesChannel->getId()][] = $categoryId;
+                }
+            }
+        }
+        $productCategoryIds = [];
+        foreach ($product->getCategories() as $productCategory) {
+            $categoryIds = array_filter(explode('|', $productCategory->getPath().'|'.$productCategory->getId()));
+            if (array_intersect($categoryIds, $this->salesChannelCategories[$salesChannel->getId()])){
+                $productCategoryIds[] = $productCategory->getId();
+            }
+        }
+        return $productCategoryIds;
+    }
+    
+    private function translate(ProductEntity $product, string $method):string
+    {
+        if (!($out = $product->{$method}())) {
+            if (!array_key_exists($product->getId(), $this->productTranslations)) {
+                $this->productTranslations[$product->getId()] = $product->getTranslations()->get($product->getId().'-'.Defaults::LANGUAGE_SYSTEM);
+            }
+            $out = $this->productTranslations[$product->getId()]->{$method}();
+        }
+        return (string) $out;
+    }
+    
+    public function export(SalesChannelContext $salesChannelContext, string $format, int $limit = null, int $offset = null): ExporterInterface
+    {
+        $context = $salesChannelContext->getContext();
+        $productsSearchResult = $this->container->get(RepositoryHelper::class)->getProducts($context, ['categories', 'translations', 'seoUrls'], [RepositoryHelper::FILTER_PRODUCT_AVAILABLE, ], $limit, $offset);
+        if ($productsSearchResult->getTotal() === 0) {
+            throw new RelevanzException("No products found.", 1585554289);
+        }
+        $exporter = $format === 'json' ? new ProductJsonExporter() : new ProductCsvExporter();
+        foreach ($productsSearchResult as $product) {
+            /* @var $product ProductEntity */
+            $exporter->addItem($this->getProductExportItem($product, $salesChannelContext));
+        }
+        return $exporter;
+    }
+    
+    private function getProductExportItem (ProductEntity $product, SalesChannelContext $salesChannelContext) {
+        /* @var $cartService CartService */
+        $domain = $salesChannelContext->getSalesChannel()->getDomains()->filterByProperty('languageId', $salesChannelContext->getContext()->getLanguageId())->first();
+        // create cart and fill with one product to get calculated price
+        $cartService = $this->container->get(CartService::class);
+        $lineItem = (new ProductLineItemFactory())->create($product->getId());
+        $cartService->add($cartService->createNew('releva'), $lineItem, $salesChannelContext);
+        $price = $priceOffer = $lineItem->getPrice()->getTotalPrice();
+        foreach ($cartService->getCart('releva', $salesChannelContext)->getLineItems()->fmap(function (LineItem $lineItem) {
+            return $lineItem->getType() === 'promotion' ? $lineItem : false;
+        }) as $promotionLineItem) {
+            $priceOffer += $promotionLineItem->getPrice()->getTotalPrice();//promotion has negative price
+        }
+        return new ProductExportItem(
+            (string) $product->getId(),
+            (array) $this->getProductCategoryIds($product, $salesChannelContext->getSalesChannel()),
+            (string) $this->translate($product, 'getName'),
+            (string) $this->translate($product, 'getMetaDescription'),
+            (string) $this->translate($product, 'getDescription'),
+            (float) $price,
+            (float) $priceOffer,
+            (string) $this->getProductUrl($product, $domain->getUrl()),
+            (string) $lineItem->getCover()->getUrl()
+         );
+    }
+    
+    private function getProductUrl(ProductEntity $product, $shopUrl): string
+    {
+        $url = null;
+        foreach ($product->getSeoUrls() as $seoUrl) {
+            $seoPathInfo = $seoUrl->getSeoPathInfo();
+            if ($seoPathInfo !== '') {
+                $url = $seoPathInfo;
+                if ($seoUrl->getIsCanonical()) {
+                    break;
+                }
+            }
+        }
+        $url = $url === null ? 'detail/'.$product->getId() : $url;
+        return $shopUrl.'/'.$url;
+    }
+    
+}
