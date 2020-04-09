0e15094d12091bb5621a7a947a7df50e01f18441
mba <mba@mba-Aspire-E5-575>
2020-04-09 08:06:43 +0200

    composer needs shopware requieres for shopware-plugin-store

--- build.sh
+++ build.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
+#clean folder
 rm -rf RelevaRetargeting RelevaRetargeting.zip
 rsync -a --exclude=nbproject --exclude=.git --exclude=.gitignore --exclude=.gitmodules --exclude=RelevaRetargeting --exclude=RelevaRetargeting.zip --exclude=.directory ./ ./RelevaRetargeting/
+
+#remove shopware-requieres
+sed -i '/"shopware\/core": "/d' ./RelevaRetargeting/composer.json
+sed -i '/"shopware\/administration": "/d' ./RelevaRetargeting/composer.json
+sed -i '/"shopware\/storefront": "/d' ./RelevaRetargeting/composer.json
+
+#install
 composer install --no-dev -n -o -d RelevaRetargeting
+
+#rollback remove requieres
+cp composer.json RelevaRetargeting
+
+#zip + clean
 zip -r RelevaRetargeting.zip RelevaRetargeting
 rm -rf RelevaRetargeting
