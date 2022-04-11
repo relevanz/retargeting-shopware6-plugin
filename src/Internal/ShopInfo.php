c4fad48de8424b769d838675cddc3947d436b9c3
mba <mba@mba-Aspire-E5-575>
2022-04-11 14:10:30 +0200

    PackageVersions\Versions is not more in shopware

--- src/Internal/ShopInfo.php
+++ src/Internal/ShopInfo.php
@@ -4,15 +4,14 @@ namespace Releva\Retargeting\Shopware\Internal;
 
 use Releva\Retargeting\Base\AbstractShopInfo;
 use Psr\Container\ContainerInterface;
-use PackageVersions\Versions;
 use Doctrine\DBAL\Connection;
 use Symfony\Component\HttpFoundation\Request;
 
 class ShopInfo extends AbstractShopInfo
 {
-    
+
     private static $container;
-    
+
     public function __construct(ContainerInterface $container)
     {
         self::$container = $container;
@@ -22,34 +21,22 @@ class ShopInfo extends AbstractShopInfo
     {
         return 'Shopware';
     }
-    
-    public static function getShopVersion(): string
+
+    public static function getShopVersion() :? string
     {
-        $versions = Versions::VERSIONS;
-        if (isset($versions['shopware/core'])) {
-            $shopwareVersion = Versions::getVersion('shopware/core');
-        } else {
-            $shopwareVersion = Versions::getVersion('shopware/platform');
-        }
-        return $shopwareVersion;
+        return self::$container->hasParameter('kernel.shopware_version') ? self::$container->getParameter('kernel.shopware_version') : null;
     }
-    
-    public static function getPluginVersion(): string
+
+    public static function getPluginVersion() :? string
     {
-        $pluginFolder = dirname(self::$container->getParameterBag()->get('kernel.bundles_metadata')['RelevaRetargeting']['path']).'/';
-        if (file_exists($pluginFolder.'vendor') && is_dir($pluginFolder.'vendor')) {// plugin have own vendor folder => its installed via admin upload
-            $version = null;
-            $composerFile = $pluginFolder.'composer.json';
-            if (file_exists($composerFile)) {
-                $composerJson = json_decode(file_get_contents($composerFile), true);
-                $version = array_key_exists('version', $composerJson) ? $composerJson['version'] : null;
+        foreach(self::$container->hasParameter('kernel.plugin_infos') ? self::$container->getParameter('kernel.plugin_infos') : [] as $pluginInfo) {
+            if ($pluginInfo['name'] === 'RelevaRetargeting') {
+                return $pluginInfo['version'];
             }
-            return $version === null ? 'unknown' : $version;
-        } else {
-            return Versions::getVersion('relevanz/retargeting-shopware-plugin');
         }
+        return null;
     }
-    
+
     public static function getDbVersion(): array
     {
         $versionData =
@@ -59,7 +46,7 @@ class ShopInfo extends AbstractShopInfo
         ;
         return empty($versionData) ? parent::getDbVersion() : $versionData;
     }
-    
+
     public static function getServerEnvironment(): array
     {
         return [
@@ -68,16 +55,16 @@ class ShopInfo extends AbstractShopInfo
             'db' => static::getDbVersion(),
         ];
     }
-    
+
     public static function getUrlCallback():? string
     {
-        return 
+        return
             self::$container === null
             ? null
             : self::$container->get('router')->getRouteCollection()->get('frontend.releva.retargeting.callback')->getPath()
         ;
     }
-    
+
     public static function getUrlProductExport():? string
     {
         return
@@ -86,5 +73,5 @@ class ShopInfo extends AbstractShopInfo
             : self::$container->get('router')->getRouteCollection()->get('frontend.releva.retargeting.products')->getPath()
         ;
     }
-    
+
 }
