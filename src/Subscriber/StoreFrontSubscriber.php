4c04d52bff6f14893242e3f46f593f7ff2ec31b3
mba <mba@mba-Aspire-E5-575>
2025-05-28 10:06:26 +0200

    compatibility SW6.7
    storefront-pixel

--- src/Subscriber/StoreFrontSubscriber.php
+++ src/Subscriber/StoreFrontSubscriber.php
@@ -2,9 +2,8 @@
 
 namespace Releva\Retargeting\Shopware\Subscriber;
 
-use Shopware\Core\Framework\Struct\ArrayEntity;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
-use Shopware\Storefront\Pagelet\Footer\FooterPageletLoadedEvent;
+use Shopware\Storefront\Event\StorefrontRenderEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 use Releva\Retargeting\Base\RelevanzApi;
@@ -21,19 +20,21 @@ class StoreFrontSubscriber implements EventSubscriberInterface
     public static function getSubscribedEvents(): array
     {
         return [
-            FooterPageletLoadedEvent::class => 'addRelevaUrls',
+            StorefrontRenderEvent::class => 'addRelevaUrls',
         ];
     }
 
-    public function addRelevaUrls(FooterPageletLoadedEvent $event): void
+    public function addRelevaUrls(StorefrontRenderEvent $event): void
     {
-        $event->getPagelet()->addExtension('releva', new ArrayEntity([
+        $headerParameters = $event->getParameter('headerParameters') ?? [];
+        $headerParameters['releva'] = [
             'tracking_active' => $this->systemConfigService->get('RelevaRetargeting.config.trackingActive', $event->getSalesChannelContext()->getSalesChannel()->getId()),
             'tracker_url' => RelevanzApi::RELEVANZ_TRACKER_URL,
             'conv_url' => RelevanzApi::RELEVANZ_CONV_URL,
             'user_id' => $this->systemConfigService->get('RelevaRetargeting.config.relevanzUserId', $event->getSalesChannelContext()->getSalesChannel()->getId()),
             'additional_html' => $this->systemConfigService->get('RelevaRetargeting.config.additionalHtml', $event->getSalesChannelContext()->getSalesChannel()->getId()),
-        ]));
+        ];
+        $event->setParameter('headerParameters', $headerParameters);
     }
     
 }
